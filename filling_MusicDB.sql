1bea3221b231853e6e347fbb17a9ca0f4c8cbcc3
pixelmagenta <eugustinova@gmail.com>
2018-10-13 18:31:41 +0300

    initial commit

---
+++ filling_MusicDB.sql
@@ -0,0 +1,32 @@
+USE MusicDB;
+
+INSERT INTO Shop (shop_id, shop_name, address) VALUES (1, "Вдохновение", "ул. Воронцовская, д. 17");
+INSERT INTO Shop (shop_id, shop_name, address) VALUES (2, "Сокровища Эвтерпы", "пер. Успенский, д. 7");
+INSERT INTO Shop (shop_id, shop_name, address) VALUES (3, "Нотный магазин", "ул. Савельева, д. 5");
+
+
+INSERT INTO Genre (genre_id, genre_name) VALUES (1, "Концерты");
+INSERT INTO Genre (genre_id, genre_name) VALUES (2, "Симфонии");
+INSERT INTO Genre (genre_id, genre_name) VALUES (3, "Сонаты");
+INSERT INTO Genre (genre_id, genre_name) VALUES (4, "Оперы");
+INSERT INTO Genre (genre_id, genre_name) VALUES (5, "Вальсы");
+INSERT INTO Genre (genre_id, genre_name) VALUES (6, "Классическая музыка");
+INSERT INTO Genre (genre_id, genre_name) VALUES (7, "Полонезы");
+INSERT INTO Genre (genre_id, genre_name) VALUES (8, "Танго");
+INSERT INTO Genre (genre_id, genre_name) VALUES (9, "Этюды");
+INSERT INTO Genre (genre_id, genre_name) VALUES (10, "Рок");
+INSERT INTO Genre (genre_id, genre_name) VALUES (11, "Романсы");
+INSERT INTO Genre (genre_id, genre_name) VALUES (12, "Блюз");
+INSERT INTO Genre (genre_id, genre_name) VALUES (13, "Джаз");
+INSERT INTO Genre (genre_id, genre_name) VALUES (14, "Поп-музыка");
+INSERT INTO Genre (genre_id, genre_name) VALUES (15, "Саундтреки");
+INSERT INTO Genre (genre_id, genre_name) VALUES (16, "Народная музыка");
+INSERT INTO Genre (genre_id, genre_name) VALUES (17, "Рапсодии");
+
+INSERT INTO Author (author_id, author_name, author_surname, author_birthdate, author_deathdate) VALUES (1, "Чарли", "Чаплин", "1889-04-16", "1977-12-25");
+INSERT INTO Author (author_id, author_name, author_surname, author_birthdate, author_deathdate) VALUES (2. "Модест", "Мусоргский", "1839-03-09", "1881-03-16");
+INSERT INTO Author (author_id, author_name, author_surname, author_birthdate, author_deathdate) VALUES (3, "Джордж", "Гершвин", "1898-09-26", "1937-07-11");
+INSERT INTO Author (author_id, author_name, author_surname, author_birthdate, author_deathdate) VALUES (4, "", "Юрима", "1978-02-15", "");
+INSERT INTO Author (author_id, author_name, author_surname, author_birthdate, author_deathdate) VALUES (5, "Доменико", "Скарлатти", "1685-10-26", "1757-07-23");
+INSERT INTO Author (author_id, author_name, author_surname, author_birthdate, author_deathdate) VALUES (6, "Джон", "Леннон", "1940-10-09", "1980-12-08");
+INSERT INTO Author (author_id, author_name, author_surname, author_birthdate, author_deathdate) VALUES (7, "Жорж", "Бизе", "1838-10-25", "1875-08-03");
